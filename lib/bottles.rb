f4c56bc5f4335de2766be5466960bf63b3f5bff3
Kerry Buckley <kerryjbuckley@gmail.com>
2016-09-24 21:03:17 +0100

    4.7: make third line match

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -11,12 +11,12 @@ class Bottles
     if number.zero?
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
         "#{quantity(number)} #{container(number)} of beer\n" +
-        "Go to the store and buy some more, " +
+        "#{action(number)}, " +
         "99 bottles of beer on the wall\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
         "#{quantity(number)} #{container(number)} of beer\n" +
-        "Take #{pronoun(number)} down and pass it around, " +
+        "#{action(number)}, " +
         "#{quantity(number - 1)} #{container(number - 1)} of beer on the wall\n"
     end
   end
@@ -44,6 +44,14 @@ class Bottles
       "it"
     else
       "one"
+    end
+  end
+
+  def action number
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(number)} down and pass it around"
     end
   end
 end
