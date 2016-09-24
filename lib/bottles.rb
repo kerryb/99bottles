57c336e98b5190b1c0b950c9ba29db047ada03e9
Kerry Buckley <kerryjbuckley@gmail.com>
2016-09-24 15:56:03 +0100

    Change from 4- to 2-line verses

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -11,24 +11,18 @@ class Bottles
     case number
     when 0 then
       <<~EOF
-        No more bottles of beer on the wall
-        No more bottles of beer
-        Go to the store and buy some more
-        99 bottles of beer on the wall
+        No more bottles of beer on the wall, no more bottles of beer
+        Go to the store and buy some more, 99 bottles of beer on the wall
       EOF
     when 1 then
       <<~EOF
-        #{number} #{container number} of beer on the wall
-        #{number} #{container number} of beer
-        Take #{pronoun number} down and pass it around
-        No more bottles of beer on the wall
+        #{number} #{container number} of beer on the wall, #{number} #{container number} of beer
+        Take #{pronoun number} down and pass it around, no more bottles of beer on the wall
       EOF
     else
       <<~EOF
-        #{number} #{container number} of beer on the wall
-        #{number} #{container number} of beer
-        Take #{pronoun number} down and pass it around
-        #{number - 1} #{container(number - 1)} of beer on the wall
+        #{number} #{container number} of beer on the wall, #{number} #{container number} of beer
+        Take #{pronoun number} down and pass it around, #{number - 1} #{container(number - 1)} of beer on the wall
       EOF
     end
   end
